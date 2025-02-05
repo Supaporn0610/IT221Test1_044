f43db1c7fcb84c80803bd6dbd7747349436d6f2e
Supaporn0610 <supaporn.i@rmutsvmail.com>
2025-02-05 09:23:55 +0700

    แก้ไขไฟล์ counter.php

--- counter.php
+++ counter.php
@@ -13,6 +13,6 @@
     $pgcount = substr($pgcount, -6);
     echo "$pgcount";
 
-    $sql = "Update counter set countnum = '$pgcount' where id = 1";
+    $sql = "Update counter set countnum = '$pgcount' where id = 1";ffgiuyh
     $result = mysqli_query($link,$sql);
 ?>
